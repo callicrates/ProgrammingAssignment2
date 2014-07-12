632597031483bcde6aac9aec29c0236dbdc870c7
Andy Wilson <atwilso@sandia.gov>
2014-07-11 21:15:21 -0600

    Implement makeCacheMatrix and cacheSolve
    OK, so far so good.  Now I just need to figure out how to make
    an invertible matrix (the identity matrix would do) so that I can
    test this.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,42 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Create a 'matrix' that can cache its own inverse
+#
+# This function is actually a little sneaky.  It creates an
+# environment that contains two values -- 'x' and 'cachedInverse' --
+# and then returns four closures that can access and modify those
+# objects.
+#
+# Is this the best way to do this in R or is this a demonstration of
+# scoping rules for the purpose of this class?
 
 makeCacheMatrix <- function(x = matrix()) {
+    set <- function(newMatrix) {
+        x <<- newMatrix
+        cachedInverse <<- NULL
+    }
+    get <- function() x
 
+    setInverse <- function(inv) {
+        cachedInverse <<- inv
+    }
+    getInverse <- function() cachedInverse
+
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Return the inverse of a matrix 'x'.  This matrix must have been
+## created using makeCacheMatrix.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    cachedValue <- x$getInverse()
+    if (!is.null(cachedValue)) {
+        message("Returning cached matrix inverse")
+        return(cachedValue)
+    }
+    newInverse = solve(x$get(), ...)
+    x$setInverse(newInverse)
+    newInverse
 }
